9a02b8df84648fbc5688b862e31c0d05a8fc8238
Kaique Almeida Real Rodrigues <ra2457024>
2025-03-24 09:19:50 -0300

    exercicio extra

---
+++ exerc4.php
@@ -0,0 +1,15 @@
+<?php
+    $MeusAmigos = [
+        ["nome" => "Davi", "city" => "Pederneiras"],
+        ["nome" => "Marco", "city" => "Pederneiras"],
+        ["nome" => "Leonardo", "city" => "Baueu"],
+        ["nome" => "Rafael", "city" => "Arealva"],
+        ["nome" => "Luiz", "city" => "Bauru"],
+        ["nome" => "Enzo", "city" => "Iacanga"],
+        ["nome" => "Samuel", "city" => "Iacanga"],
+        ["nome" => "Eduardo", "city" => "Iacanga"],
+    ];
+
+    echo $MeusAmigos[3]['nome'];
+    echo $MeusAmigos[3]['city'];
+?>
